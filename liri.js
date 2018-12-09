065e7e15d192f4254b14cde79882f3c167efc437
adoration29 <Adora Okogeri>
2018-12-08 23:37:21 -0500

    uncomplete

---
+++ liri.js
@@ -0,0 +1,14 @@
+
+require("dotenv").config();
+var spotifyCredentials = require('./keys.js').spotify;
+
+var Spotify= require('node-spotify-api');
+var request = require('request');
+var moment = require('moment');
+//this stores the key to a variable
+var spotify = new Spotify(keys.spotify);
+
+ 
+
+   
+  
